626a056303e2988ff76296730a68e185d74ca19f
MananAlchemy <manan@alchemytech.ca>
2024-01-20 20:12:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import UseContextDemo from "./UseContext/UseContextDemo";
+// import Demo from "./UseMemo and Callback/Demo";
+// import UseRefDemo from "./UseRef/UseRef";
+// import UseReducerDemo from "./UseReducer/UseReducerDemo";
+// import Main from "./UseEffect/Main";
+// import Main from "./UseState/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>{/* <Demo /> */}</div>;
 }
 
 export default App;
